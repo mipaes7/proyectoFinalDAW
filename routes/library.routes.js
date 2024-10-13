d4502faff26ce0329163eefae538748a11b62841
Miguel Pardal <miguelpardalesparis@gmail.com>
2024-10-13 19:32:48 +0200

    library routes added

---
+++ routes/library.routes.js
@@ -0,0 +1,19 @@
+const libraryController = require('../controllers/library.controllers');
+// const authMiddleware = require('../middlewares/authMiddleware');
+const router = require('express').Router();
+
+// GET http://localhost:3000/api/libraries
+// GET http://localhost:3000/api/libraries?email=exampleuser@example.com
+router.get('/', libraryController.readLibrariesController);
+
+// POST http://localhost:3000/api/libraries
+router.post('/', libraryController.createLibraryEntryController);
+
+// PUT http://localhost:3000/api/libraries
+router.put('/', libraryController.updateLibraryEntryStatusController);
+
+// DELETE http://localhost:3000/api/libraries
+router.delete('/', libraryController.deleteEntryController);
+
+module.exports = router;
+
